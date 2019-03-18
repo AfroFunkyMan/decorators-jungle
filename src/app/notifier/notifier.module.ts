303b97e91fc227224243e9ab3d73bed1fb960ba8
Ruslan <afro.funky.lover@gmail.com>
2019-03-18 12:02:07 +0700

    add notifier

---
+++ src/app/notifier/notifier.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotifierComponent } from './notifier.component';
+
+@NgModule({
+  declarations: [NotifierComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NotifierModule { }
